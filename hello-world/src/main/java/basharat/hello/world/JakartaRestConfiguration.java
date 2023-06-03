a471688ee3676e948e75f2334e8dfb3ac48fd601
Ray7705 <rehmanbasharat7705@outlook.com>
2023-06-03 14:28:20 -0400

    Hello World

---
+++ hello-world/src/main/java/basharat/hello/world/JakartaRestConfiguration.java
@@ -0,0 +1,13 @@
+package basharat.hello.world;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+/**
+ * Configures Jakarta RESTful Web Services for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JakartaRestConfiguration extends Application {
+    
+}
