aefa7873da6f79262f6a7d398d5139d32a7f8f8a
Ray7705 <rehmanbasharat7705@outlook.com>
2023-06-03 17:27:07 -0400

    Formatting (in class)

---
+++ formatting/src/main/java/basharat/formatting/formatting.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package basharat.formatting;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ *
+ * @author rehma
+ */
+
+
+
+
+
