360aca1387460a4154a5b36e7f371bb241b739e4
Ray7705 <rehmanbasharat7705@outlook.com>
2023-06-08 17:47:04 -0400

    Parameters Method POST

--- parameters/src/main/java/basharat/Form/SignUpController.java
+++ parameters/src/main/java/basharat/Form/SignUpController.java
@@ -17,7 +17,10 @@ import java.io.PrintWriter;
  * @author rehma
  */
 @WebServlet(name = "SignUpControllerServlet", urlPatterns = {"/SignUp"})
+
 public class SignUpController extends HttpServlet {
+/*
+
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
@@ -60,7 +63,49 @@ public class SignUpController extends HttpServlet {
                 out.println("No Semester");
             }
         }
+    
+    }
+*/
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+                String firstName = request.getParameter("firstname");
+        String lastName = request.getParameter("firstname");
+        String[] courses = request.getParameterValues("course");
+        String time = request.getParameter("time");
+        String [] semesters = request.getParameterValues( "semester");
         
+        try(PrintWriter out = response.getWriter()){
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<body>");   
+         
+            out.println("First Name: " + firstName);
+            out.println("Last Name: " + lastName);
+            
+            if (courses != null) {
+                for (String course : courses) {
+                    out.println("<p>Courses: " + course + "</p>");
+                }
+            }
+            else{
+                out.println("No Course");
+            }
+            
+            if (time != null) {
+                out.println("<p>Time: " + time + "</p>");
+            }
+            else{
+                out.println("No Time");
+            }
+            if (semesters != null) {
+                for (String semester : semesters) {
+                    out.println("<p>Semesters: " + semester + "</p>");
+                }
+            }
+            else{
+                out.println("No Semester");
+            }
+        }
     }
     
 }
