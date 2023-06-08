62ea65d0ab53806b571081161883e57861419c09
Ray7705 <rehmanbasharat7705@outlook.com>
2023-06-08 17:42:51 -0400

    Update Students-1 1.0 (Displaying the students)

--- Students-1/src/main/java/Students/Controller/StudentListController.java
+++ Students-1/src/main/java/Students/Controller/StudentListController.java
@@ -13,6 +13,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import Students.Model.Student;
 import Students.Repository.StudentRepository;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 
 /**
@@ -24,28 +25,33 @@ public class StudentListController extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        PrintWriter out = response.getWriter();
         
-        StudentRepository studentRepository = new StudentRepository();
         
-        ArrayList<Student> allStudents = studentRepository.getStudents();
+        StudentRepository repository = new StudentRepository();
+        ArrayList<Student> students = repository.getStudents();
         
-        out.println("<!DOCTYPE html>");
-        out.println("<html>");
-        out.println("<head>");
-        out.println("<title>Stocks</title>");
-        out.println("</head>");
-        out.println("<body>");
+        // write to the response (headers and body)
+        response. setContentType( "text/html; charset-UTF-8" );
+        DateTimeFormatter dateFormatter = DateTimeFormatter .ofPattern( "yyyy-MM-dd");
         
-        for (int i = 0; i < 5; i++) {
-            out.println("<ul>");
-            //out.println("<li>" + studentRepository.toString()+ "</li>");
-            out.println("<li>"  + "</li>"); 
-            out.println("</ul>");
-        }
-        out.println("</body>");
-        out.println("</html>");
-        out.flush();
+        try(PrintWriter out = response.getWriter()) {
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Students</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Students</h1>");
+            for (Student student : students) {
+                out.println("<ul>");
+                out.println("<li>" + student.toString() + "</li>");
+                out.println("<li>Date Of Birth: " + student.getDateOfBirth() + "</li>");
+                out.println("<li>Age: " + dateFormatter.format(student.getDateOfBirth())+ "</li>");
+                out.println("</ul>");
+            }
+            out.println("</body>");
+            out.println("</html>");
+            out.flush();
+        } 
     }
-    
 }
