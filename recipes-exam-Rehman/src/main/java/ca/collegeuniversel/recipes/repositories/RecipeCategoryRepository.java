36a940c2ea2576a1e4f8542b03e658ea81e27cfd
Ray7705 <rehmanbasharat7705@outlook.com>
2023-07-07 17:01:53 -0400

    Recipes Exam Repositories

--- recipes-exam-Rehman/src/main/java/ca/collegeuniversel/recipes/repositories/RecipeCategoryRepository.java
+++ recipes-exam-Rehman/src/main/java/ca/collegeuniversel/recipes/repositories/RecipeCategoryRepository.java
@@ -1,39 +1,51 @@
 package ca.collegeuniversel.recipes.repositories;
 
 import ca.collegeuniversel.recipes.entities.RecipeCategory;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
-public class RecipeCategoryRepository
-{
+public class RecipeCategoryRepository {
+
     private final String connectionUrl;
     private final String connectionUsername;
     private final String connectionPassword;
 
-    public RecipeCategoryRepository()
-    {
+    public RecipeCategoryRepository() {
         String databaseName = "recipes_db";
         connectionUrl = "jdbc:mariadb://localhost:3315/" + databaseName;
         connectionUsername = "root";
         connectionPassword = "admin";
     }
-    
-    public RecipeCategory getCategory(int id) throws ClassNotFoundException, SQLException
-    {
+
+    public RecipeCategory getCategory(int id) throws ClassNotFoundException, SQLException {
         // TODO
         return null;
     }
 
-    public ArrayList<RecipeCategory> getCategories() throws ClassNotFoundException, SQLException
-    {
-        // TODO
-        return null;
+    public ArrayList<RecipeCategory> getCategories() throws ClassNotFoundException, SQLException {
+        Class.forName("org.mariadb.jdbc.Driver");
+        try (Connection connection = DriverManager.getConnection(connectionUrl, connectionUsername, connectionPassword)) {
+            String query = "SELECT id, name, image_path FROM categories";
+            PreparedStatement statement = connection.prepareStatement(query);
+            ResultSet resultSet = statement.executeQuery();
+            ArrayList<RecipeCategory> categories = new ArrayList<>();
+            while (resultSet.next()) {
+                categories.add(readNextCategory(resultSet));
+            }
+            return categories;
+        }
+
     }
 
-    private static RecipeCategory readNextCategory(ResultSet resultSet) throws SQLException
-    {
-        // TODO
-        return null;
+    private static RecipeCategory readNextCategory(ResultSet resultSet) throws SQLException {
+        int id = resultSet.getInt("id");
+        String name = resultSet.getString("name");
+        String plural_name = resultSet.getString("image_path");
+
+        return new RecipeCategory(id, name, plural_name);
     }
 }
--- recipes-exam-Rehman/src/main/java/ca/collegeuniversel/recipes/repositories/RecipeRepository.java
+++ recipes-exam-Rehman/src/main/java/ca/collegeuniversel/recipes/repositories/RecipeRepository.java
@@ -2,84 +2,85 @@ package ca.collegeuniversel.recipes.repositories;
 
 import ca.collegeuniversel.recipes.entities.Recipe;
 import ca.collegeuniversel.recipes.entities.RecipeCategory;
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
-public class RecipeRepository
-{
+public class RecipeRepository {
+
     private final String connectionUrl;
     private final String connectionUsername;
     private final String connectionPassword;
 
-    public RecipeRepository()
-    {        
+    public RecipeRepository() {
         String databaseName = "recipes_db";
         connectionUrl = "jdbc:mariadb://localhost:3315/" + databaseName;
         connectionUsername = "root";
         connectionPassword = "admin";
     }
-    
-    public Recipe getRecipe(int id) throws ClassNotFoundException, SQLException
-    {
+
+    public Recipe getRecipe(int id) throws ClassNotFoundException, SQLException {
+        Class.forName("org.mariadb.jdbc.Driver");
+        try (Connection connection = DriverManager.getConnection(connectionUrl, connectionUsername, connectionPassword)) {
+            String query = "SELECT id, name, description, image_path, category_id, FROM recipes WHERE id = ?";
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.setInt(1, id);
+            ResultSet resultSet = statement.executeQuery();
+            ArrayList<Recipe> recipes = new ArrayList<>();
+            while (resultSet.next()) {
+                recipes.add(readNextRecipe(resultSet));
+            }
+            return recipes.get(0);
+        }
+    }
+
+    public ArrayList<Recipe> getRecipes() throws ClassNotFoundException, SQLException {
         // TODO
         return null;
     }
-    
-    public ArrayList<Recipe> getRecipes() throws ClassNotFoundException, SQLException
-    {
-        // TODO
-        return null;
-    }
-    
-    public ArrayList<Recipe> getRecipesByCategory(RecipeCategory category) throws ClassNotFoundException, SQLException
-    {
+
+    public ArrayList<Recipe> getRecipesByCategory(RecipeCategory category) throws ClassNotFoundException, SQLException {
         return getRecipesByCategory(category.getId());
     }
-    
-    public ArrayList<Recipe> getRecipesByCategory(int categoryId) throws ClassNotFoundException, SQLException
-    {
+
+    public ArrayList<Recipe> getRecipesByCategory(int categoryId) throws ClassNotFoundException, SQLException {
         // TODO
         return null;
     }
-    
-    public ArrayList<Recipe> getLikedRecipes(String username) throws ClassNotFoundException, SQLException
-    {
+
+    public ArrayList<Recipe> getLikedRecipes(String username) throws ClassNotFoundException, SQLException {
         // TODO
         return null;
     }
-    
-    private static Recipe readNextRecipe(ResultSet resultSet) throws SQLException
-    {
+
+    private static Recipe readNextRecipe(ResultSet resultSet) throws SQLException {
         // TODO
         return null;
     }
-    
-    public Recipe addRecipe(Recipe recipe) throws ClassNotFoundException, SQLException
-    {
+
+    public Recipe addRecipe(Recipe recipe) throws ClassNotFoundException, SQLException {
         // TODO
         return null;
     }
-    
-    private int getGeneratedId(PreparedStatement statement) throws SQLException
-    {
-        try (ResultSet generatedKeys = statement.getGeneratedKeys())
-        {
-            if (generatedKeys.next())
+
+    private int getGeneratedId(PreparedStatement statement) throws SQLException {
+        try (ResultSet generatedKeys = statement.getGeneratedKeys()) {
+            if (generatedKeys.next()) {
                 return generatedKeys.getInt(1);
+            }
             throw new SQLException("Failed to read generated id.");
         }
     }
-    
-    public boolean addLike(String username, int recipeId) throws ClassNotFoundException, SQLException
-    {
+
+    public boolean addLike(String username, int recipeId) throws ClassNotFoundException, SQLException {
         // TODO
         return false;
     }
-    
-    public boolean deleteLike(String username, int recipeId) throws ClassNotFoundException, SQLException
-    {
+
+    public boolean deleteLike(String username, int recipeId) throws ClassNotFoundException, SQLException {
         // TODO
         return false;
     }
